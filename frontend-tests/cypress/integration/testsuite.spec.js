56eb01a9af8438f2a337cbc42fd500041c99b5f0
Stina Renberg <stinarenberg1@gmail.com>
2020-10-22 10:55:48 +0200

    Created testproject and .gitignore

---
+++ frontend-tests/cypress/integration/testsuite.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+//This is a testsuite
+describe ('Assignment 01 testsuite', function(){
+    //This is a testcase
+    it('TC01 - Perform valid login', function(){
+        cy.visit('http://localhost:3000')
+
+
+    })
+
+
+
+})
